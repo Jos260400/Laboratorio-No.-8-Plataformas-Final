e766dac98cfce1705fb623317297369cc78670b5
José Ovando <42974480+Jos260400@users.noreply.github.com>
2019-05-20 23:38:58 -0600

    Add files via upload

---
+++ src/main/java/com/example/inventario/Lista.kt
@@ -0,0 +1,105 @@
+package com.example.inventario
+
+import android.app.Activity
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.*
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_lista.*
+import android.R.attr.data
+import android.support.annotation.NonNull
+import android.support.v7.widget.helper.ItemTouchHelper
+import android.widget.Toast
+import androidx.navigation.ui.NavigationUI.*
+import com.google.zxing.integration.android.IntentResult
+
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class Lista : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+        val binding:com.example.inventario.databinding.FragmentListaBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_lista, container, false)
+        
+        
+        
+        
+        var lista = Inventario.globalInventario
+        binding.listaItems.layoutManager= LinearLayoutManager(context, LinearLayout.VERTICAL,false)
+        val ada = Adaptador(lista)
+        binding.listaItems.adapter=ada
+        setHasOptionsMenu(true)
+        
+        
+        
+        
+        
+        /*
+        Eliminar un producto de toda nuestra mercadería.
+         */
+        var helper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(@NonNull recyclerView: RecyclerView, @NonNull viewHolder: RecyclerView.ViewHolder, @NonNull viewHolder1: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+            override fun onSwiped(@NonNull target: RecyclerView.ViewHolder, p1:Int){
+                val position = target.getAdapterPosition()
+                lista.removeAt(position)
+                binding.listaItems.adapter?.notifyDataSetChanged()
+            }
+        })
+        helper.attachToRecyclerView(binding.listaItems)
+
+        binding.boton.setOnClickListener(){
+            val scanner = IntentIntegrator(activity)
+            scanner.initiateScan()
+        }
+        return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId!!.equals(R.id.reiniciar)){
+            var lista = Inventario.globalInventario
+            lista.clear()
+            listaItems.adapter?.notifyDataSetChanged()
+        }
+        if(item.itemId.equals(R.id.actual)){
+            Toast.makeText(context, "Usted tiene varios productos", Toast.LENGTH_LONG).show()
+        }
+        return onNavDestinationSelected(
+            item,
+            view!!.findNavController()
+        )
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(context, "Cancelado", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context, "Escaneado: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/example/inventario/Nuevo.kt
@@ -0,0 +1,41 @@
+package com.example.inventario
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class Nuevo : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
+
+        val binding: com.example.inventario.databinding.FragmentNuevoBinding= DataBindingUtil.inflate(
+            inflater, R.layout.fragment_nuevo, container, false
+        )
+
+
+
+        binding.add.setOnClickListener(){ view:View->
+            val nuenom = binding.editText.text.toString()
+            val nueid = binding.editText2.text.toString()
+            var lista = Inventario.globalInventario
+            val pro = Producto(nuenom,nueid)
+            var fi = Carta(pro,0)
+            lista.add(fi)
+            Navigation.findNavController(view).navigate(R.id.action_nuevo_to_fragmentoLista)
+        }
+        return binding.root
+    }
+}
